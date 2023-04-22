45b062b324ad6b445f14bfa9359fec7f0febff84
Sukrut Patil <34018683+SukrutPatil@users.noreply.github.com>
2023-04-22 16:01:23 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const navSide = () =>{
+    const burger =  document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle')
+    })
+    navLinks.forEach((link, index)=>{
+        link.style.animation = `navlinksFade 0.7s ease forwards ${index/ 7 + 1}s`
+    })
+    
+}
+navSide()
